2da0438ba78ab9864d6bac1b19125affb9372d78
andreachiabotto <andrea.chiabotto@generali.com>
2022-06-22 14:48:54 +0200

    feat(snake): added game over function;

--- index.js
+++ index.js
@@ -1,89 +1,103 @@
 "use strict";
-const rowNumber = 10;
-const colNumber = 10;
+const rowNumber = 20;
+const colNumber = 30;
 const cellWidth = 30;
 const cellHeight = 30;
-const cellText = '+';
-const foodText = ['🦋', '🐛', '🐝', '🐞', '🐜', '🕷', '🦂', '🦗', '🦟'];
-const snakeText = '🐍';
-let direction = 'right';
-const container = document.getElementById('container');
+const cellText = "+";
+const foodText = [
+    "🦋",
+    "🐛",
+    "🐝",
+    "🐞",
+    "🐜",
+    "🕷",
+    "🦂",
+    "🦗",
+    "🦟",
+];
+const snakeText = "🐍";
+let direction = "right";
+let points = 0;
+const container = document.getElementById("container");
 // FOOD
-const food = document.createElement('div');
-food.id = 'food';
+const food = document.getElementById("food");
+let foodPosition = { top: 0, left: 0 };
+food.style.width = cellWidth.toString() + "px";
+food.style.height = cellHeight.toString() + "px";
 // SNAKE
-let snake = document.createElement('div');
+const snake = document.getElementById("snake");
 let snakeLeftPosition = colNumber / 2;
 let snakeTopPosition = rowNumber / 2;
-let snakePositions = [{ top: snakeTopPosition, left: snakeLeftPosition }];
-snake.id = 'snake';
-container.appendChild(snake);
+let snakePositions = [
+    { top: snakeTopPosition, left: snakeLeftPosition },
+];
+// speed of the game
+let speed = 300;
 for (let r = 0; r < rowNumber; r++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
+    const row = document.createElement("div");
+    row.classList.add("row");
     container.appendChild(row);
     for (let c = 0; c < colNumber; c++) {
-        const currentRow = document.querySelectorAll('.row');
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.style.width = cellWidth.toString() + 'px';
-        cell.style.height = cellHeight.toString() + 'px';
+        const currentRow = document.querySelectorAll(".row");
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.style.width = cellWidth.toString() + "px";
+        cell.style.height = cellHeight.toString() + "px";
         cell.innerHTML = `<p>${cellText}</p>`;
         currentRow[r].appendChild(cell);
     }
 }
-addFood();
-moveFood();
-// this function add a div with id 'food' to the container
-function addFood() {
-    food.style.width = cellWidth.toString() + 'px';
-    food.style.height = cellHeight.toString() + 'px';
-    food.innerHTML = `<p>${foodText[Math.floor(Math.random() * foodText.length)]}</p>`;
-    container.appendChild(food);
+startGame();
+function startGame() {
+    moveFood();
+    addSnake();
+    moveSnake();
 }
 // this function move the food to a random position
 function moveFood() {
-    food.style.top = (Math.floor(Math.random() * rowNumber) * cellHeight).toString() + 'px';
-    food.style.left = (Math.floor(Math.random() * colNumber) * cellWidth).toString() + 'px';
+    const top = Math.floor(Math.random() * rowNumber);
+    const left = Math.floor(Math.random() * colNumber);
+    food.innerHTML = `<p>${foodText[Math.floor(Math.random() * foodText.length)]}</p>`;
+    foodPosition = { top: top, left: left };
+    food.style.top = (top * cellHeight).toString() + "px";
+    food.style.left = (left * cellWidth).toString() + "px";
 }
-addSnake();
 // this function add a div with class 'snake' to the container
 function addSnake() {
-    snake.innerHTML = '';
+    snake.innerHTML = "";
     // generate a snake part looping trough the snakePositions array
     for (let i = 0; i < snakePositions.length; i++) {
-        const snakePart = document.createElement('div');
-        snakePart.className = 'snake' + i;
-        snakePart.style.top = snakePositions[i].top * cellHeight + 'px';
-        snakePart.style.left = snakePositions[i].left * cellWidth + 'px';
+        const snakePart = document.createElement("div");
+        snakePart.className = "snake" + i;
+        snakePart.style.top = snakePositions[i].top * cellHeight + "px";
+        snakePart.style.left = snakePositions[i].left * cellWidth + "px";
         snakePart.innerHTML = `<p>${snakeText}</p>`;
         snake.appendChild(snakePart);
     }
 }
-moveSnake();
 // this function move the snake to a random position
 function moveSnake() {
     // moving snake following the direction
     switch (direction) {
-        case 'right':
+        case "right":
             snakeLeftPosition++;
             if (snakeLeftPosition >= colNumber) {
                 snakeLeftPosition = 0;
             }
             break;
-        case 'left':
+        case "left":
             snakeLeftPosition--;
             if (snakeLeftPosition < 0) {
                 snakeLeftPosition = colNumber - 1;
             }
             break;
-        case 'up':
+        case "up":
             snakeTopPosition--;
             if (snakeTopPosition < 0) {
                 snakeTopPosition = rowNumber - 1;
             }
             break;
-        case 'down':
+        case "down":
             snakeTopPosition++;
             if (snakeTopPosition >= rowNumber) {
                 snakeTopPosition = 0;
@@ -91,27 +105,65 @@ function moveSnake() {
             break;
         default: // do nothing
     }
-    snakePositions = [{ top: snakeTopPosition, left: snakeLeftPosition }, ...snakePositions];
+    snakePositions = [
+        { top: snakeTopPosition, left: snakeLeftPosition },
+        ...snakePositions,
+    ];
     snakePositions.pop();
-    // addSnake();
+    //check if the snake eats itself
+    snakeEatsItself();
+    //check if the snake eat the food
+    snakeEatsFood();
+    addSnake();
 }
 // add EventListener of type keyEvent to the document to check if arrow keys are pressed
-document.addEventListener('keydown', (e) => {
+document.addEventListener("keydown", (e) => {
     e.preventDefault();
-    if (e.key === 'ArrowUp' && direction !== 'down') {
-        direction = 'up';
+    if (e.key === "ArrowUp" && direction !== "down") {
+        direction = "up";
     }
-    else if (e.key === 'ArrowDown' && direction !== 'up') {
-        direction = 'down';
+    else if (e.key === "ArrowDown" && direction !== "up") {
+        direction = "down";
     }
-    else if (e.key === 'ArrowLeft' && direction !== 'right') {
-        direction = 'left';
+    else if (e.key === "ArrowLeft" && direction !== "right") {
+        direction = "left";
     }
-    else if (e.key === 'ArrowRight' && direction !== 'left') {
-        direction = 'right';
+    else if (e.key === "ArrowRight" && direction !== "left") {
+        direction = "right";
     }
 });
-// every 200ms the snake will move to the next position
+// this function update the score
+function updateScore() {
+    const score = document.getElementById("score");
+    const random = Math.floor(Math.random() * 5) + 5;
+    points += random;
+    score.innerHTML = `<p>Score: ${points}</p>`;
+}
+function snakeEatsFood() {
+    if (getCollision(snakePositions[0].top, snakePositions[0].left, foodPosition.top, foodPosition.left)) {
+        moveFood();
+        snakePositions = [
+            ...snakePositions,
+            { top: snakeTopPosition, left: snakeLeftPosition },
+        ];
+        updateScore();
+        speed -= 5;
+    }
+}
+function snakeEatsItself() {
+    const snakeHead = snakePositions[0];
+    const snakeBody = snakePositions.slice(1);
+    const headEatBody = snakeBody.find((part) => part.top === (snakeHead === null || snakeHead === void 0 ? void 0 : snakeHead.top) && part.left === snakeHead.left);
+    if (headEatBody) {
+        alert("Game over!");
+        points = 0;
+        startGame();
+    }
+}
+function getCollision(snakeTop, snakeLeft, objTop, objLeft) {
+    return snakeLeft === objLeft && snakeTop === objTop;
+}
+// every x time the snake will move to the next position
 setInterval(() => {
     moveSnake();
-}, 500);
+}, speed);
--- index.ts
+++ index.ts
@@ -1,140 +1,216 @@
-const rowNumber: number = 10;
-const colNumber: number = 10;
+const rowNumber: number = 20;
+const colNumber: number = 30;
 
 const cellWidth: number = 30;
 const cellHeight: number = 30;
 
-const cellText: string = '+';
-const foodText: Array<string> = ['🦋', '🐛', '🐝', '🐞', '🐜', '🕷', '🦂', '🦗', '🦟'];
-const snakeText: string = '🐍';
+const cellText: string = "+";
+const foodText: Array<string> = [
+  "🦋",
+  "🐛",
+  "🐝",
+  "🐞",
+  "🐜",
+  "🕷",
+  "🦂",
+  "🦗",
+  "🦟",
+];
+const snakeText: string = "🐍";
 
-let direction: Directions = 'right';
+let direction: Directions = "right";
+let points: number = 0;
 
-const container = document.getElementById('container');
+const container = document.getElementById("container");
 // FOOD
-const food: HTMLElement = document.createElement('div');
-food.id = 'food';
+const food = document.getElementById("food");
+let foodPosition: Position = { top: 0, left: 0 };
+food!.style.width = cellWidth.toString() + "px";
+food!.style.height = cellHeight.toString() + "px";
+
 // SNAKE
-let snake: HTMLElement = document.createElement('div');
+const snake = document.getElementById("snake");
 let snakeLeftPosition: number = colNumber / 2;
 let snakeTopPosition: number = rowNumber / 2;
-let snakePositions: Array<Position> = [{top: snakeTopPosition, left: snakeLeftPosition}];
-snake.id = 'snake';
-container!.appendChild(snake);
+let snakePositions: Array<Position> = [
+  { top: snakeTopPosition, left: snakeLeftPosition },
+];
+
+// speed of the game
+let speed: number = 300;
 
 for (let r = 0; r < rowNumber; r++) {
-    const row: HTMLElement = document.createElement('div');
-    row.classList.add('row');
-    container!.appendChild(row);
-    for (let c = 0; c < colNumber; c++) {
-        const currentRow = document.querySelectorAll('.row');
-        const cell: HTMLElement = document.createElement('div');
-        cell.classList.add('cell');
-        cell.style.width = cellWidth.toString() + 'px';
-        cell.style.height = cellHeight.toString() + 'px';
-        cell.innerHTML = `<p>${cellText}</p>`;
-        currentRow[r]!.appendChild(cell);
-    }
+  const row: HTMLElement = document.createElement("div");
+  row.classList.add("row");
+  container!.appendChild(row);
+  for (let c = 0; c < colNumber; c++) {
+    const currentRow = document.querySelectorAll(".row");
+    const cell: HTMLElement = document.createElement("div");
+    cell.classList.add("cell");
+    cell.style.width = cellWidth.toString() + "px";
+    cell.style.height = cellHeight.toString() + "px";
+    cell.innerHTML = `<p>${cellText}</p>`;
+    currentRow[r]!.appendChild(cell);
+  }
 }
 
-addFood();
-moveFood();
+startGame();
 
-// this function add a div with id 'food' to the container
-function addFood() {
-    food.style.width = cellWidth.toString() + 'px';
-    food.style.height = cellHeight.toString() + 'px';
-    food.innerHTML = `<p>${foodText[Math.floor(Math.random() * foodText.length)]}</p>`;
-    container!.appendChild(food);
+function startGame() {
+  moveFood();
+  addSnake();
+  moveSnake();
 }
 
 // this function move the food to a random position
 function moveFood() {
-    food.style.top = (Math.floor(Math.random() * rowNumber) * cellHeight).toString() + 'px';
-    food.style.left = (Math.floor(Math.random() * colNumber) * cellWidth).toString() + 'px';
-}
+  const top = Math.floor(Math.random() * rowNumber);
+  const left = Math.floor(Math.random() * colNumber);
+  food!.innerHTML = `<p>${
+    foodText[Math.floor(Math.random() * foodText.length)]
+  }</p>`;
 
-addSnake();
+  foodPosition = { top: top, left: left };
+
+  food!.style.top = (top * cellHeight).toString() + "px";
+  food!.style.left = (left * cellWidth).toString() + "px";
+}
 
 // this function add a div with class 'snake' to the container
 function addSnake() {
-    snake.innerHTML = '';
+  snake!.innerHTML = "";
 
-    // generate a snake part looping trough the snakePositions array
-    for (let i = 0; i < snakePositions.length; i++) {
-        const snakePart: HTMLElement = document.createElement('div');
-        snakePart.className = 'snake' + i;
-        snakePart.style.top = snakePositions[i]!.top * cellHeight + 'px';
-        snakePart.style.left = snakePositions[i]!.left * cellWidth + 'px';
-        snakePart.innerHTML = `<p>${snakeText}</p>`;
-        snake.appendChild(snakePart);
-    }
+  // generate a snake part looping trough the snakePositions array
+  for (let i = 0; i < snakePositions.length; i++) {
+    const snakePart: HTMLElement = document.createElement("div");
+    snakePart.className = "snake" + i;
+    snakePart.style.top = snakePositions[i]!.top * cellHeight + "px";
+    snakePart.style.left = snakePositions[i]!.left * cellWidth + "px";
+    snakePart.innerHTML = `<p>${snakeText}</p>`;
+    snake!.appendChild(snakePart);
+  }
 }
-
-moveSnake();
 
 // this function move the snake to a random position
 function moveSnake() {
-    // moving snake following the direction
-    switch (direction) {
-        case 'right':
-            snakeLeftPosition++;
-            if ( snakeLeftPosition >= colNumber ) {
-                snakeLeftPosition = 0;
-            }
-            break;
-        case 'left':
-            snakeLeftPosition--;
-            if ( snakeLeftPosition < 0 ) {
-                snakeLeftPosition = colNumber - 1;
-            }
-            break;
-        case 'up':
-            snakeTopPosition--;
-            if( snakeTopPosition < 0 ) {
-                snakeTopPosition = rowNumber - 1;
-            }
-            break;
-        case 'down':
-            snakeTopPosition++;
-            if( snakeTopPosition >= rowNumber ) {
-                snakeTopPosition = 0;
-            }
-            break;
-        default:   // do nothing
-    }
+  // moving snake following the direction
+  switch (direction) {
+    case "right":
+      snakeLeftPosition++;
+      if (snakeLeftPosition >= colNumber) {
+        snakeLeftPosition = 0;
+      }
+      break;
+    case "left":
+      snakeLeftPosition--;
+      if (snakeLeftPosition < 0) {
+        snakeLeftPosition = colNumber - 1;
+      }
+      break;
+    case "up":
+      snakeTopPosition--;
+      if (snakeTopPosition < 0) {
+        snakeTopPosition = rowNumber - 1;
+      }
+      break;
+    case "down":
+      snakeTopPosition++;
+      if (snakeTopPosition >= rowNumber) {
+        snakeTopPosition = 0;
+      }
+      break;
+    default: // do nothing
+  }
 
-    snakePositions = [{top: snakeTopPosition, left: snakeLeftPosition}, ...snakePositions];
-    snakePositions.pop();
+  snakePositions = [
+    { top: snakeTopPosition, left: snakeLeftPosition },
+    ...snakePositions,
+  ];
+  snakePositions.pop();
 
-   // addSnake();
+  //check if the snake eats itself
+  snakeEatsItself();
+  //check if the snake eat the food
+  snakeEatsFood();
+
+  addSnake();
 }
 
 // add EventListener of type keyEvent to the document to check if arrow keys are pressed
-document.addEventListener('keydown', (e: KeyboardEvent) => {
-    e.preventDefault();
+document.addEventListener("keydown", (e: KeyboardEvent) => {
+  e.preventDefault();
 
-    if (e.key === 'ArrowUp' && direction !== 'down') {
-        direction = 'up';
-    } else if (e.key === 'ArrowDown' && direction !== 'up') {
-        direction = 'down';
-    } else if (e.key === 'ArrowLeft' && direction !== 'right') {
-        direction = 'left';
-    } else if (e.key === 'ArrowRight' && direction !== 'left') {
-        direction = 'right';
-    }
+  if (e.key === "ArrowUp" && direction !== "down") {
+    direction = "up";
+  } else if (e.key === "ArrowDown" && direction !== "up") {
+    direction = "down";
+  } else if (e.key === "ArrowLeft" && direction !== "right") {
+    direction = "left";
+  } else if (e.key === "ArrowRight" && direction !== "left") {
+    direction = "right";
+  }
 });
 
-// every 200ms the snake will move to the next position
-setInterval(() => {
-    moveSnake();
-}, 500);
+// this function update the score
+function updateScore() {
+  const score = document.getElementById("score");
+  const random = Math.floor(Math.random() * 5) + 5;
+  points += random;
+  score!.innerHTML = `<p>Score: ${points}</p>`;
+}
 
+function snakeEatsFood() {
+  if (
+    getCollision(
+      snakePositions[0]!.top,
+      snakePositions[0]!.left,
+      foodPosition.top,
+      foodPosition.left
+    )
+  ) {
+    moveFood();
+    snakePositions = [
+      ...snakePositions,
+      { top: snakeTopPosition, left: snakeLeftPosition },
+    ];
+    updateScore();
+    speed -= 5;
+  }
+}
+
+function snakeEatsItself() {
+  const snakeHead = snakePositions[0];
+  const snakeBody = snakePositions.slice(1);
+
+  const headEatBody = snakeBody.find(
+    (part) => part.top === snakeHead?.top && part.left === snakeHead.left
+  );
+
+  if (headEatBody) {
+    alert("Game over!");
+    points = 0;
+    startGame();
+  }
+}
+
+function getCollision(
+  snakeTop: number,
+  snakeLeft: number,
+  objTop: number,
+  objLeft: number
+): boolean {
+  return snakeLeft === objLeft && snakeTop === objTop;
+}
+
+// every x time the snake will move to the next position
+setInterval(() => {
+  moveSnake();
+}, speed);
 
 // NEW TYPE / INTERFACES
-type Directions = 'right' | 'left' | 'up' | 'down';
+type Directions = "right" | "left" | "up" | "down";
 
 interface Position {
-    top: number;
-    left: number;
+  top: number;
+  left: number;
 }
